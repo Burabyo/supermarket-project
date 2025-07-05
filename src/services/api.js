58737dcb64a4de097b6a38a54345c64ace942809
Burabyo <j.burabyo@alustudent.com>
2025-07-05 09:34:58 +0200

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://supermarket-project-production.up.railway.app/api/';
+const API_BASE_URL = 'https://supermarket-project-production.up.railway.app/api';
 
 // Create axios instance
 const api = axios.create({
