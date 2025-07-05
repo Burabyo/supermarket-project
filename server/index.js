5eee889bfed2e66d98515a636fa2820a36234cd5
Burabyo <j.burabyo@alustudent.com>
2025-07-05 11:45:06 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // or restrict to your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
